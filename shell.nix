228621a298548db25f35ff50615bd72c4d4bcd09
Tilde Rose <t1lde@protonmail.com>
2020-11-04 19:37:03 +0000

    nix build

---
+++ shell.nix
@@ -0,0 +1,20 @@
+{nixpkgs ? import <nixpkgs> { }, ghc ? nixpkgs.ghc}:
+
+with nixpkgs;
+
+let
+    dontCheck = pkgs.haskell.lib.dontCheck;
+    haskellPackages_ = haskellPackages.override {
+        overrides = self: super: {
+                   twitter-conduit = dontCheck (super.twitter-conduit);
+        };
+    };
+in
+pkgs.haskell.lib.buildStackProject {
+    name = "tweetdelete";
+    buildInputs = [
+        haskellPackages_.twitter-conduit
+        haskellPackages_.twitter-types
+        haskellPackages_.twitter-types-lens
+    ];
+}
