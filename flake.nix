8e16fe7352ac76b058cd66e3dfbd692ceb875080
Tilde Rose <t1lde@protonmail.com>
2022-03-11 21:45:45 +0000

    flake

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "tweetdump";
+
+  inputs.haskell-nix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskell-nix/nixpkgs";
+
+  outputs = inputs@{ self, nixpkgs, haskell-nix, haskell-nix, ... }:
+    let
+      supportedSystems = with nixpkgs.lib.systems.supported; tier1 ++ tier2 ++ tier3;
+
+      perSystem = nixpkgs.lib.genAttrs supportedSystems;
+
+      nixpkgsFor = system: import nixpkgs { inherit system; overlays = [ haskell-nix.overlay ]; inherit (haskell-nix) config; };
+      nixpkgsFor' = system: import nixpkgs { inherit system; inherit (haskell-nix) config; };
+
+      ghcVersion = "ghc8107";
+
+      tools = {};
+
+      projectFor = system:
+        let pkgs = nixpkgsFor system; in
+        let pkgs' = nixpkgsFor' system; in
+        (nixpkgsFor system).haskell-nix.cabalProject' {
+          src = ./.;
+          compiler-nix-name = ghcVersion;
+          ## extraSources = [
+          ##   {
+          ##     src = inputs.etc;
+          ##     subdirs = [
+          ##     ];
+          ##   }
+          ## ];
+          modules = [ (plutarch.haskellModule system) ];
+          shell = {
+            withHoogle = true;
+
+            exactDeps = true;
+
+            # We use the ones from Nixpkgs, since they are cached reliably.
+            # Eventually we will probably want to build these with haskell.nix.
+            nativeBuildInputs = [];
+
+            inherit tools;
+
+            additional = ps: [
+            ];
+          };
+        };
+
+    in
+    {
+      project = perSystem projectFor;
+      flake = perSystem (system: (projectFor system).flake { });
+
+      packages = perSystem (system: self.flake.${system}.packages);
+      devShell = perSystem (system: self.flake.${system}.devShell);
+    };
+}
